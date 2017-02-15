15a3fc0720b7a1050949f8c16687125d1f11df65
Crossley <m_crossley@tss-hr420-dm02.local>
2017-02-15 12:57:45 -0500

    Fixing up js files for geolocation

--- js/main.js
+++ js/main.js
@@ -1,21 +1,30 @@
 (function(){
-	var map, marker;
+	var map = new google.maps.Map(document.querySelector('.map-wrapper')), marker;
 
-	function initMap(){
+	function initMap(position){
 
-		map = new google.maps.Map(document.querySelector('.map-wrapper'),
-		{
-			center: {lat: 42.983233, lng: -81.250688},
-			zoom: 14
+		map.setCenter({
+			position: {lat: position.coords.latitude, lng: position.coords.longitude}
 		});
 
+		map.setZoom(16);
+
 		marker = new google.maps.Marker({
-			position: {lat: 42.983233, lng: -81.250688},
+			position: {lat: position.coords.latitude, lng: position.coords.longitude},
 			map: map,
 			title: 'Oh hi...'
 		});
 	}
 
-	initMap();
+	if(navigator.geolocation){
+		navigator.geolocation.getCurrentPosition(initMap, handleError);
+	}else{
+		//Give some kind of error message to the user
+		console.log('Sorry, your browser does not support geolocation');
+	}
+
+	function handleError(e){
+		console.log(e);
+	}
 
 })();
