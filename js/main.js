c22d7358551140e8278a82c54ce4b8d94dfc138d
mvcrossley <mcrossl2@uwo.ca>
2017-03-01 13:05:26 -0500

    Adding map functionality and UX

--- js/main.js
+++ js/main.js
@@ -1,7 +1,20 @@
 (function(){
-	var map = new google.maps.Map(document.querySelector('.map-wrapper')), marker;
+	var map = new google.maps.Map(document.querySelector('.map-wrapper')), 
+	marker,
+	preloader = document.querySelector('.preloadWrapper'),
+	geocoder = new google.maps.Geocoder(),
+	geocodeButton = document.querySelector('.geocode'),
+
+	//directions services
+	directionService = new google.maps.DirectionsService(),
+	directionsDisplay,
+	locations = [];
 
 	function initMap(position){
+		locations[0] = {lat: position.coords.latitude, lng: position.coords.longitude};
+
+		directionsDisplay = new google.maps.DirectionsRenderer();
+		directionsDisplay.setMap(map); //setMap lets you know what map the API should target
 
 		map.setCenter({lat: position.coords.latitude, lng: position.coords.longitude});
 
@@ -11,6 +24,49 @@
 			position: {lat: position.coords.latitude, lng: position.coords.longitude},
 			map: map,
 			title: 'Oh hi...'
+		});
+
+		preloader.classList.add('hide-preloader');
+	}
+
+	function codeAddress()
+	{
+		//debugger;
+		var address = document.querySelector('.address').value;
+		console.log(address);
+
+		geocoder.geocode({'address' : address}, function(results, status){
+			if (status === google.maps.GeocoderStatus.OK) // if The Geocoder came back valid
+			{
+				locations[1] = {lat: results[0].geometry.location.lat(), lng: results[0].geometry.location.lng()};
+				map.setCenter(results[0].geometry.location);//object index 0, inside that geometry object, inside that, a location
+
+				if (marker){
+					marker.setMap(null);
+					marker = new google.maps.Marker({
+						map: map,
+						position: results[0].geometry.location
+					})
+				}
+
+				calcRoute(results[0].geometry.location);
+			}else{
+				console.log("Geocoder was unsuccessful. Error:", status);
+			}
+		});
+	}
+
+	function calcRoute(codedLoc){
+		var request = {
+			origin: locations[0],
+			destination: locations[1],
+			travelMode: 'DRIVING'
+		};
+
+		directionService.route(request, function(response, status) {
+			if (status === 'OK'){
+				directionsDisplay.setDirections(response);
+			}
 		});
 	}
 
@@ -24,5 +80,7 @@
 	function handleError(e){
 		console.log(e);
 	}
+
+	geocodeButton.addEventListener('click', codeAddress, false);
 
 })();
